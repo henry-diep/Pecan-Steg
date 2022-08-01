982343ab10a3ce309cbdad938bcbe29f1898abd0
henry-diep <77194238+henry-diep@users.noreply.github.com>
2022-08-01 18:25:59 +0800

    Delete steg_test.py

--- steg_test.py
+++
@@ -1,5 +0,0 @@
-from stegano import lsb
-secret = lsb.hide("./watching.png", ".. .----. -- / .-- .- - -.-. .... .. -. --. / -.-- --- ..-")
-secret.save("./watching_secret.png")
-clear_message = lsb.reveal("./watching_secret.png")
-print(clear_message)
